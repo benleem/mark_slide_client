197750923d878198977fd153e6a6bb9775384ccb
voidKandy <voidkandy@gmail.com>
2023-08-14 11:16:20 -0700

    shows/profile page updates to reflect ShowView changes

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import { PUBLIC_API_BASE_URL } from '$env/static/public';
     import ShowView from '$lib/components/ShowView.svelte';
+    import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
     import type { PageData } from './$types';
     import { goto } from '$app/navigation'
     import ProfileItem from '$lib/components/ProfileItem.svelte';
     import { convertTime } from "$lib/utils/helpers";
+    import type { Show } from "$lib/models/shows";
     
     export let data: PageData
 
@@ -19,6 +22,32 @@
         }
     }
 
+    const shows = writable<Show[]>([]);
+    const getShows = async () => {
+        const response = await fetch(`${PUBLIC_API_BASE_URL}/shows/users/${data.user.id}?favorites=false`, {
+            credentials: "include",
+            headers: { "Authorization": `Bearer ${data.token}` }
+        });
+        const responseJson = await response.json();
+        if (responseJson.status === "success") {
+            const showData = responseJson.data.shows; 
+
+            const mappedShows: Show[] = showData.map(show => ({
+                id: show.id,
+                user_id: show.user_id,
+                title: show.title,
+                description: show.description,
+                view_code: show.view_code,
+                is_public: show.public,
+                created_at: new Date(show.created_at),
+                updated_at: new Date(show.updated_at)
+            }));
+            
+            shows.set(mappedShows);
+        
+        }
+    };
+   onMount(getShows);
 </script>
 
 <section class="h-full w-full flex flex-col items-center justify-center">
@@ -42,5 +71,5 @@
     {:else if !data.user || data.error}
         <p class="text-red-500">{data.error}</p>
     {/if}
-    <ShowView data={data}/>
+    <ShowView shows={shows}/>
 </section>
--- src/routes/shows/+page.server.ts
+++ src/routes/shows/+page.server.ts
@@ -1,9 +1,28 @@
 import type { PageServerLoad } from "./$types";
+import { PUBLIC_API_BASE_URL } from "$env/static/public";
 
-export const load: PageServerLoad = async () => {
-	return {
-		text: "Shows page "
-	};
+export const load: PageServerLoad = async ({ fetch, cookies }) => {
+	try {
+		const token = cookies.get("token");
+		const response = await fetch(`${PUBLIC_API_BASE_URL}/auth/current_user`);
+		const responseJson = await response.json();
+
+		if (!responseJson.data) {
+			return {
+				error: responseJson.message
+			};
+		}
+
+		const user = responseJson.data.user;
+		return {
+			user,
+			token
+		};
+	} catch (error) {
+		if (error instanceof Error) {
+			return { error: error.message };
+		}
+	}
 };
 
 
