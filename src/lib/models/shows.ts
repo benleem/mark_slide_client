758ae2faadc7edc1115f444a91a6a1e05c036232
voidKandy <voidkandy@gmail.com>
2023-08-14 11:15:57 -0700

    shows type

---
+++ src/lib/models/shows.ts
@@ -0,0 +1,10 @@
+export type Show = {
+    id: String,
+    user_id: String,
+    title: String,
+    description: String,
+    view_code: String,
+    is_public: boolean,
+    created_at: Date,
+    updated_at: Date
+}
